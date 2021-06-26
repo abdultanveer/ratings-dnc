fd04015b8626505bb36f249d4638b1c64eb07ceb
abdul <abdul.tanveer@gmail.com>
2021-06-26 15:30:06 +0530

    ratingsResource returns a UserRating object containing a list of ratings for a that user

--- Ratings-dnc/src/main/java/dnc/sp/ms/ratings/resources/RatingsResource.java
+++ Ratings-dnc/src/main/java/dnc/sp/ms/ratings/resources/RatingsResource.java
@@ -1,10 +1,14 @@
 package dnc.sp.ms.ratings.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dnc.sp.ms.ratings.model.Rating;
+import dnc.sp.ms.ratings.model.UserRating;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,7 +16,22 @@ public class RatingsResource {
 	
 	@RequestMapping("/{movieId}")
 	public Rating getRating(@PathVariable("movieId") String movieId) {
-		return new Rating("movieid1", 1);
+		return new Rating("movieid1", 1);	
+	}
+	
+	@RequestMapping("users/{userId}")
+	public UserRating getUserRating(@PathVariable("userId") String userId) {
+		List<Rating> ratingsBag = Arrays.asList(
+				new Rating("1234",4),
+				new Rating("5678",3)				
+				);
+		UserRating userRating = new UserRating();
+		userRating.setUserRatings(ratingsBag);
+		return userRating;
+		
 	}
 
 }
+
+
+		
